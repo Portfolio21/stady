0fa5a7cb6e59d24c2db719c79120df95cfba1cca
gruzd-di <gruzd.di@gbksoft.com>
2021-11-14 18:52:17 +0200

    add all elements

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'hello world';
+require 'vendor/autoload.php';
+
+$checker = new \Gruzd\Tools\Url\HostChecker();
+
+$targetUrl = 'https://ithillel.ua/';
+
+if ($checker->check($targetUrl)) {
+    $guzzle = new GuzzleHttp\Client();
+    $content = $guzzle->get($targetUrl);
+    echo $content->getBody()->getContents();
+} else {
+    echo 'The requested host is not available';
+}
